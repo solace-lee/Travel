cd809e215169c6736c0da3ea7e2827b7c3962004
李祖明 <solace.lee@outlook.com>
2018-08-14 22:13:08 +0800

    HomeHeader

--- src/views/Home/components/Header.vue
+++ src/views/Home/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="header">
     <div class="header-left">返回</div>
-    <div class="header-input">内容</div>
+    <div class="header-input">输入城市/景点/游玩主题</div>
     <div class="header-right">城市</div>
   </div>
 </template>
@@ -16,14 +16,23 @@ export default {
 <style scoped lang="stylus">
 .header
  display: flex
- height: .86rem
+ line-height: .86rem
  background: #00bcd4
+ color: #fff
  .header-left
   width: .64rem
   float: left
  .header-input
   flex: 1
+  height: .64rem
+  line-height: .64rem
+  margin-top: .12rem
+  margin-left: .2rem
+  background: #fff
+  border-radius: .1rem
+  color: #ccc
  .header-right
   width: 1.24rem
   float: right
+  text-align: center
 </style>
