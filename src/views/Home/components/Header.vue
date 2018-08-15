539ab5f7668f297426e57a70c5dd1006d3c491d4
solacelee <solace.lee@outlook.com>
2018-08-15 14:14:55 +0800

    引入iconfont

--- src/views/Home/components/Header.vue
+++ src/views/Home/components/Header.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="header">
-    <div class="header-left">返回</div>
-    <div class="header-input">输入城市/景点/游玩主题</div>
-    <div class="header-right">城市</div>
+    <div class="header-left">
+    	<div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+    	<span class="iconfont">&#xe632;</span>
+    	输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+    	城市
+    	<span class="arrow-icon"></span>
+    </div>
   </div>
 </template>
 
@@ -14,20 +22,25 @@ export default {
 </script>
 
 <style scoped lang="stylus">
+@import '../../../assets/styles/varibles.styl'
 .header
  display: flex
  line-height: .86rem
- background: #00bcd4
+ background: $bgColor
  color: #fff
  .header-left
   width: .64rem
   float: left
+  .back-icon
+   text-align: center
+   font-size: .4rem
  .header-input
   flex: 1
   height: .64rem
   line-height: .64rem
   margin-top: .12rem
   margin-left: .2rem
+  padding-left: .2rem
   background: #fff
   border-radius: .1rem
   color: #ccc
@@ -35,4 +48,11 @@ export default {
   width: 1.24rem
   float: right
   text-align: center
+  .arrow-icon
+   display: inline-block
+   width: 0
+   height: 0
+   border-width: .14rem .14rem 0
+   border-style: solid
+   border-color: #fff transparent transparent
 </style>
