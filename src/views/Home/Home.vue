5ebaec21c7b08b82330716224ae258b501c7555f
李祖明 <solace.lee@outlook.com>
2018-08-15 22:58:29 +0800

    index-icons

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <home-header></home-header>
     <home-swiper></home-swiper>
+    <home-icons></home-icons>
     <di>test</di>
   </div>
 </template>
@@ -10,12 +11,14 @@
 // @ is an alias to /src
 import HomeHeader from './components/Header.vue'
 import HomeSwiper from './components/Swiper.vue'
+import HomeIcons from './components/Icons.vue'
 
 export default {
   name: 'home',
   components: {
     HomeHeader,
-    HomeSwiper
+    HomeSwiper,
+    HomeIcons
   }
 }
 </script>
