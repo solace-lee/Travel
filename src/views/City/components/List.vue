804e126a6ebfb5e62f0cfa5d9794169dd0e2a3fc
solacelee <solace.lee@outlook.com>
2018-08-23 17:17:32 +0800

    city-list

---
+++ src/views/City/components/List.vue
@@ -0,0 +1,61 @@
+<template lang="html">
+  <div class="list">
+    <div class="area">
+      <div class="title border-topbottom">当前城市</div>
+      <div class="button-list">
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">热门城市</div>
+      <div class="button-list">
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+      </div>
+      <div class="button-list">
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+      </div>
+    </div>
+    <div class="area">
+      <div class="title border-topbottom">A</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityList'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '../../../assets/styles/varibles.styl'
+.border-topbottom
+  &:before
+    border-color: #ccc
+  &:after
+    border-color: #ccc
+.title
+  line-height: .44rem
+  background: #eee
+  padding-left: .2rem
+  font-size: .26rem
+  color: #666
+.button-list
+  overflow: hidden
+  padding: .1rem .6rem .1rem .1rem
+  .button-wrapper
+    float: left
+    width: 33.33%
+    .button
+      margin: .1rem
+      padding: .1rem
+      text-align: center
+      border: .02rem solid #ccc
+      border-radius: .06rem
+</style>
