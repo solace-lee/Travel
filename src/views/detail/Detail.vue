bb21429a287f867bf283133325a01fa731372f64
solacelee <solace.lee@outlook.com>
2018-08-27 17:09:13 +0800

    组件的递归

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -3,7 +3,7 @@
     <detail-banner></detail-banner>
     <detail-header></detail-header>
     <div class="content">
-
+      <detail-list :list="list"></detail-list>
     </div>
   </div>
 </template>
@@ -11,11 +11,36 @@
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
   name: 'Detail',
   components: {
     DetailHeader,
-    DetailBanner
+    DetailBanner,
+    DetailList
+  },
+  data () {
+    return {
+      list: [{
+        title: '成人票',
+        children: [{
+          title: '成人三馆联票',
+          children: [{
+            title: '成人五馆联票'
+          }, {
+            title: '成人六馆联票'
+          }]
+        }, {
+          title: '成人四馆联票'
+        }]
+      }, {
+        title: '学生票'
+      }, {
+        title: '儿童票'
+      }, {
+        title: '特惠票'
+      }]
+    }
   }
 }
 </script>
